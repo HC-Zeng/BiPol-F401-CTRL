af91b00e433ae15fec22dad1b4763dedc57b0305
huacai <ty101059@qq.com>
2025-11-10 20:16:56 +0800

    V2 1、补充文件

---
+++ cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/DependInfo.cmake
@@ -0,0 +1,88 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Core/Startup/startup_stm32f401rctx.s" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Core/Startup/startup_stm32f401rctx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F401xC"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Core/Src/LCD.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Core/Src/LCD.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Core/Src/adc.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Core/Src/adc.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Core/Src/crc.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Core/Src/crc.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Core/Src/dma.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Core/Src/dma.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Core/Src/flash_store.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Core/Src/flash_store.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Core/Src/gpio.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Core/Src/gpio.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Core/Src/main.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Core/Src/main.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Core/Src/spi.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Core/Src/spi.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Core/Src/stm32f4xx_hal_msp.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Core/Src/stm32f4xx_it.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Core/Src/stm32f4xx_it.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Core/Src/syscalls.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Core/Src/syscalls.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Core/Src/sysmem.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Core/Src/sysmem.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Core/Src/system_stm32f4xx.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Core/Src/system_stm32f4xx.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Core/Src/tim.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Core/Src/tim.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Core/Src/usart.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Core/Src/usart.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_crc.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_crc.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj"
+  "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_adc.c" "C:/Users/HC/CLionProjects/BiPol-F401-CTRL/cmake-build-debug/CMakeFiles/BiPol-F401-CTRL.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_adc.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  "STM32F401xC"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
